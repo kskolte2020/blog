5e03a58489781810369afa89711b31c893abbda9
Kiran Kolte <kiran.kolte@fournxt.com>
2022-12-01 20:14:15 +0530

    Created an Post App

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+    include Visible
+  
+    has_many :comments, dependent: :destroy
+  
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
+  end
+  
